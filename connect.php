64b64a7f4dd0fd407694d69b5ba8a7ffaa71146c
OriginalYan <sokoliko999@gmail.com>
2019-07-27 21:47:18 +0300

    Продакшн

--- connect.php
+++ connect.php
@@ -2,16 +2,16 @@
 
 //данные для подключения к бд откуда будут браться таблицы
 $connect_from = array(
-    'DSN' => 'mysql:host=bitrix-new.devinotest.local;port=3306;dbname=bitrix24prod;charset=utf8;',
-    'PASS' => 'bitrix24prod',
-    'LOGIN' => 'bitrix24prod',
-    'DB_NAME' => 'bitrix24prod'
+    'DSN' => 'mysql:host=bitrix-new.devinotest.local;port=3306;dbname=;charset=utf8;',
+    'PASS' => '',
+    'LOGIN' => '',
+    'DB_NAME' => ''
 );
 
 //данные для подключения к бд куда будут они копироваться
 $connect_to = array(
-    'DSN' => 'mysql:host=bitrix-new.devinotest.local;port=3306;dbname=bitrix24;charset=utf8;',
-    'PASS' => 'bitrix24',
-    'LOGIN' => 'bitrix24',
-    'DB_NAME' => 'bitrix24'
+    'DSN' => 'mysql:host=bitrix-new.devinotest.local;port=3306;dbname=;charset=utf8;',
+    'PASS' => '',
+    'LOGIN' => '',
+    'DB_NAME' => ''
 );
